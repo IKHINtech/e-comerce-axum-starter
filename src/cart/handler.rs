abdc497b071a940974836cc0b0db0ddef5d7abe3
IKHINtech <sarikhinlovestory@gmail.com>
2025-11-28 17:24:42 +0700

    [update] add model cart

--- src/cart/handler.rs
+++ src/cart/handler.rs
@@ -1,6 +1,10 @@
 use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
+use sqlx::PgPool;
 
-use crate::{cart::model::AddToChartRequest, shared::auth_guard::JWTAuth};
+use crate::{
+    cart::model::{AddToChartRequest, CartItemDTO},
+    shared::auth_guard::JWTAuth,
+};
 
 pub async fn add_to_chat_handler(
     State(pool): State<sqlx::PgPool>,
@@ -30,5 +34,34 @@ pub async fn add_to_chat_handler(
             // Error handling jika product_id tidak ada di tabel products
             (StatusCode::INTERNAL_SERVER_ERROR, format!("Gagal: {}", e)).into_response()
         }
+    }
+}
+
+pub async fn get_cart_handler(State(pool): State<PgPool>, claims: JWTAuth) -> impl IntoResponse {
+    // QUERY JOIN
+    // Mengambil data cart + nama produk + hitung total harga
+    let items = sqlx::query_as!(
+        CartItemDTO,
+        r#"
+        SELECT 
+            c.id as cart_id,
+            p.id as product_id,
+            p.name as product_name,
+            p.price,
+            c.quantity,
+            (p.price * c.quantity) as "total_price!" -- Tanda ! memberitahu sqlx ini pasti tidak null
+        FROM cart_items c
+        JOIN products p ON c.product_id = p.id
+        WHERE c.user_id = $1
+        ORDER BY c.id ASC
+        "#,
+        claims.user_id
+    )
+    .fetch_all(&pool)
+    .await;
+
+    match items {
+        Ok(data) => (StatusCode::OK, Json(data)).into_response(),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, format!("{}", e)).into_response(),
     }
 }
